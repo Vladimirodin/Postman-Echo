ad0e64b0a8b5f926ef2234b1e3436f2e2294fe03
Vladimir Odintcev <vovaodintcev@gmail.com>
2022-12-13 11:46:55 +0500

    modified

--- src/test/java/ru/netology/rest/MobileBankApiTestV7.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV7.java
@@ -2,10 +2,9 @@ package ru.netology.rest;
 
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
-import io.restassured.http.ContentType;
+
 import static io.restassured.RestAssured.given;
-import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
 import static org.hamcrest.Matchers.*;
 
 class MobileBankApiTestV7 {
@@ -16,13 +15,13 @@ class MobileBankApiTestV7 {
         given()
                 .baseUri("https://postman-echo.com")
                 .contentType("text/plain; charset=UTF-8")
-                .body("some value") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some value"));
+                .body("data", equalTo("some data"));
     }
 }
